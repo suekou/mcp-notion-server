cee6b089c2b82fa6e9092be5b784dc246ddf967f
Kosuke Suenaga <suekou2004_0904@yahoo.co.jp>
2025-05-14 09:51:32 +0900

    Update Dependencies and Fix Tests
    * Update `@modelcontextprotocol/sdk` to version `1.11.2`
    * Add `node-fetch` with version `2.7.0`
    * Replace `global.fetch` with `fetch` in test files and mock `node-fetch`
    * Update `package-lock.json` and `package.json`, and add `@types/node-fetch` to devDependencies

--- src/client/index.ts
+++ src/client/index.ts
@@ -14,6 +14,7 @@ import {
   RichTextItemResponse,
   CreateDatabaseArgs,
 } from "../types/index.js";
+import fetch from "node-fetch";
 
 export class NotionClientWrapper {
   private notionToken: string;
